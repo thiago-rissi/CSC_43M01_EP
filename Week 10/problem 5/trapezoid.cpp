04fe0980c05a2f7e6c166bb1a66d974be769f3cf
Thiago Lizier Rissi <thiago.rissi@usp.br>
2026-05-26 18:56:28 +0200

    Dijkstra

--- Week 10/problem 5/trapezoid.cpp
+++ Week 10/problem 5/trapezoid.cpp
@@ -73,3 +73,71 @@ int main() {
 
     return 0;
 }
+
+// #include <climits>
+// #include <iostream>
+// #include <queue>
+// #include <vector>
+// using namespace std;
+
+// vector<int> dijkstra(vector<vector<pair<int, int>>> &adj, int src) {
+
+//     int V = adj.size();
+
+//     // Min-heap (priority queue) storing pairs of (distance, node)
+//     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+
+//     vector<int> dist(V, INT_MAX);
+
+//     // Distance from source to itself is 0
+//     dist[src] = 0;
+//     pq.emplace(0, src);
+
+//     // Process the queue until all reachable vertices are finalized
+//     while (!pq.empty()) {
+//         auto top = pq.top();
+//         pq.pop();
+
+//         int d = top.first;
+//         int u = top.second;
+
+//         // If this distance not the latest shortest one, skip it
+//         if (d > dist[u])
+//             continue;
+
+//         // Explore all neighbors of the current vertex
+//         for (auto &p : adj[u]) {
+//             int v = p.first;
+//             int w = p.second;
+
+//             // If we found a shorter path to v through u, update it
+//             if (dist[u] + w < dist[v]) {
+//                 dist[v] = dist[u] + w;
+//                 pq.emplace(dist[v], v);
+//             }
+//         }
+//     }
+
+//     // Return the final shortest distances from the source
+//     return dist;
+// }
+
+// int main() {
+//     int src = 0;
+
+//     vector<vector<pair<int, int>>> adj(5);
+//     adj[0] = {{1, 4}, {2, 8}};
+//     adj[1] = {{0, 4}, {4, 6}, {2, 3}};
+//     adj[2] = {{0, 8}, {3, 2}, {1, 3}};
+//     adj[3] = {{2, 2}, {4, 10}};
+//     adj[4] = {{1, 6}, {3, 10}};
+
+//     vector<int> result = dijkstra(adj, src);
+
+//     for (int d : result)
+//         cout << d << " ";
+//     cout << "
+//             ";
+
+//         return 0;
+// }
